8097dab0726757f65b51141cfc2d1e88ddb2f21d
Alice Hsiao <alice.hsiao87@gmail.com>
2018-09-12 11:05:36 -0700

    Tested and implemented words method

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,1 +1,14 @@
+def words(word)
+  word_hash = {}
 
+  individual_words = word.split(" ")
+  individual_words.each do |item|
+    if word_hash[item] == nil
+      word_hash[item] = 1
+    else
+      word_hash[item] += 1
+    end
+  end
+
+  return word_hash
+end
